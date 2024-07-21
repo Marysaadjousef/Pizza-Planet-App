db259b878de7b2df7995815d6cb961c48e2bed82
Mary Saad <92622235+Marysaadjousef@users.noreply.github.com>
2024-07-21 21:20:47 +0300

    Delete lib directory

--- lib/constants/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color red = const Color.fromRGBO(249, 30, 59, 1);
-Color darkGray = const Color(0xff292929);
-Color black = const Color(0xff121212);
-
-bool darkMode = false;
